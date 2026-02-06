c5a9db7a8615d144e5982af2ab57b986b4dc4b5b
admin <admin@adminui-MacBookPro.local>
2026-02-06 17:35:32 +0900

    React Router 과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import "./App.css";
 import headerStyle from "./assets/styles/header.module.css";
 /* Layout 컴포넌트
@@ -8,16 +9,29 @@ import headerStyle from "./assets/styles/header.module.css";
   4-1. 무작위 경로를 할당하거나, Math.random()을 활용할 수 있습니다.
 5. Layout 컴포넌트는 "모든" 컴포넌트에서 보여야 합니다. <Outlet/>를 추가하세요.
 */
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ErrorPage from "./pages/ErrorPage";
+
 function Layout() {
   return (
     <>
       <header className={headerStyle.header}>
         <div>Router Practice</div>
+
         <div className={headerStyle.headerLinks}>
           {/* 여기에 Link 컴포넌트를 할당하세요. */}
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to={`/random-${Math.random()}`}>Random</Link>
         </div>
       </header>
-      {/* 이곳에 Outlet을 할당하세요. */}
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
@@ -33,7 +47,19 @@ function Layout() {
 8. /* 모든 잘못된 라우팅에서 보이는 <ErrorPage/> 컴포넌트를 라우팅에 연결하세요.
 */
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
